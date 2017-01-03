588e9097809f307a017d8e63b3fb1aa4fd159e67
TWNateLo <mmartin20tw@gmail.com>
2017-01-03 08:36:16 +0800

    Create hw7_2.c

---
+++ hw7/hw7_2.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+double power(double a,int b);
+void itob(int i);	//int to binary
+void ftob(float f);	//float to binary
+void btof(char bb);	//binary to float
+union flt2bin{
+	float f;
+	unsigned u;
+};
+struct bin2flt{
+	unsigned sign:1;
+	long expo:8;
+	unsigned mantisa:24;
+};
+
+int main(int argc, char *argv[]){
+	if(atoi(argv[1]) == 1) {
+		itob(atoi(argv[2]));
+	}
+	if(atoi(argv[1]) == 2) {
+		ftob(atof(argv[2]));
+	}
+	//define the special char type here in the btof section or just pass the value in first
+	if(atoi(argv[1]) == 3)	{
+		//try to input argv[2] string to a char
+		char bina[] = ;
+		btof(argv[2]);
+	}
+}
+
+void itob(int i) {
+	printf("under construction");
+}
+
+void ftob(float f){
+	union flt2bin f2b;
+	unsigned max = 0x80000000;
+	f2b = f;
+	while(max){
+		printf("%d",(f2b.u & max) != 0);
+		max>>=1;
+	}		
+	printf("\n");
+}
+void btof(char bb){
+	char bin[sizeof(float)*8];
+	int v=0;
+	float mantisa=0;
+	int sign;
+	struct bin2flt b2f;
+	float  result;
+	bin = bb;
+	for(int i=0;i<32;i++){
+		v += (bin[i] - '0')*power(2,31-i);
+	}
+	b2f.sign = (v >> 31);
+	b2f.expo = (v >> 23) - 127;
+	b2f.mantisa =  v & 0x007FFFFF;
+	sign = (b2f.sign==1)? -1:1;
+	mantisa = (float)(b2f.mantisa + 0x00800000)/0x00800000;
+	result = (float)(sign)*(float)power(2,b2f.expo)*mantisa;
+	printf("%e\n", result);
+}
+double power(double a,int b){
+	if(b==0) return 1;
+	if(b>0)	return a*power(a,b-1);
+	if(b<0)	return (1/a)*power(a,b+1);
+}
